95d921516c4495762c992484064211f09bd81951
kw cho <ckw7407@naver.com>
2022-07-15 11:55:16 +0900

    write form create

---
+++ src/com/mission/javabeans/BoardBean.java
@@ -0,0 +1,14 @@
+package com.mission.javabeans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardBean {
+	private String name;
+	private String pass;
+	private String email;
+	private String title;
+	private String content;
+}
---
+++ src/com/saeyan/javabeans/MemberBean.java
@@ -0,0 +1,17 @@
+package com.saeyan.javabeans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberBean {
+	private String name; // 이름을 저장할 필드 선언
+	private String userid; // 아이디를 저장할 필드 선언
+	private String nickname; // 별명을 저장할 필드 선언
+	private String pwd; // 비밀번호를 저장할 필드 선언
+	private String email; // 이메일을 저장할 필드 선언
+	private String phone; // 전화번호를 저장할 필드 선언
+	
+	
+}
